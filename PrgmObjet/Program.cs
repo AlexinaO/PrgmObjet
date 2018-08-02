2148d07913a5a3cbad019fc02327dc2912facb6f
AlexinaO <alexina.orsoni@gmail.com>
2018-08-02 12:49:26 +0200

    Classes

--- PrgmObjet/Program.cs
+++ PrgmObjet/Program.cs
@@ -10,6 +10,27 @@ namespace PrgmObjet
     {
         static void Main(string[] args)
         {
+            var personne1 = new Personne("Franck");
+            var personne2 = new Personne("Franck","Dupont");
+            var personne3 = new Personne("Franck","Dupont", "Toto");
+
+            var date = new DateTime();
         }
     }
+    class Personne
+    {
+        public Personne(string nom)
+        {
+
+        }
+        public Personne(string nom, string prenom)
+        {
+
+        }
+        public Personne (string nom, string prenom, string surnom)
+        {
+
+        }
+    }
+
 }
